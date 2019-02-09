aa15207dde919e8e3c7605275d983146b4aef348
jhu369 <jhu369@gatech.edu>
2019-02-09 13:57:25 -0600

    finish phase1 rasterer

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -47,30 +47,24 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        boolean r =checkInput(params);
         Map<String, Object> results = new HashMap<>();
-
-        String[][] grid = new String[][]{};
-        double ul_lon = 0;
-        double ul_lat = 0;
-        double lr_lon = 0;
-        double lr_lat = 0;
-        int depth =0;
-        Boolean isSuccess = false;
-
-
-
-        results.put("render_grid",grid);
-        results.put("raster_ul_lon",ul_lon);
-        results.put("raster_ul_lat",ul_lat);
-        results.put("raster_lr_lon",lr_lon);
-        results.put("raster_lr_lat",lr_lat);
-        results.put("depth",depth);
-        results.put("query_success",isSuccess);
+        results.put("render_grid",getGrid(params,getDepth(params)));
+        results.put("raster_ul_lon",getRaster(params,"ullon"));
+        results.put("raster_ul_lat",getRaster(params,"ullat"));
+        results.put("raster_lr_lon",getRaster(params,"lrlon"));
+        results.put("raster_lr_lat",getRaster(params,"lrlat"));
+        results.put("depth",getDepth(params));
+        results.put("render_grid",getGrid(params,getDepth(params)));
+        results.put("query_success",isSuccess(results,params));
         return results;
     }
 
-    private static int getDepth(double ullon, double lrlon, double w){
+    //get depth of the figure with right lonDPP
+    private static int getDepth(Map<String, Double> params){
+        double ullon = params.get("ullon");
+        double lrlon = params.get("lrlon");
+        double w = params.get("w");
         double lonDPP = (lrlon -ullon)/w;
         double depth = Math.ceil(Math.log(ROOT_LonDPP/lonDPP) / Math.log(2));
         if(depth < 0) depth = 0;
@@ -78,24 +72,96 @@ public class Rasterer {
         return (int)depth;
     }
 
-    private static double[] getRaster(double ullon, double ul_lat, double lrlon, double lr_lat, double w){
-        double lonCellSize = (ROOT_LRLON - ROOT_ULLON)/(getDepth(ullon,lrlon,w) + 1);
-        double latCellSize = (ROOT_ULLAT - ROOT_LRLAT)/(getDepth(ullon,lrlon,w) + 1);
+    //get the ul and lr index, in rang[0, 2^depth]
+    private static int[] getGridXY(Map<String, Double> params){
+        double ullon = params.get("ullon");
+        double ullat = params.get("ullat");
+        double lrlon = params.get("lrlon");
+        double lrlat = params.get("lrlat");
+        int depth = getDepth(params);
+        double lonCellSize = (ROOT_LRLON - ROOT_ULLON)/Math.pow(2,depth);
+        double latCellSize = (ROOT_ULLAT - ROOT_LRLAT)/Math.pow(2,depth);
         int ulx = (int) Math.floor((ullon - ROOT_ULLON) / lonCellSize);
-        int uly = (int) Math.floor((ROOT_ULLAT - ul_lat) / latCellSize);
+        int uly = (int) Math.floor((ROOT_ULLAT - ullat) / latCellSize);
         int lrx = (int) Math.floor((lrlon - ROOT_ULLON) / lonCellSize);
-        int lry = (int) Math.floor((ROOT_ULLAT - lr_lat) / lonCellSize);
+        int lry = (int) Math.floor((ROOT_ULLAT - lrlat) / latCellSize);
+        int[] gridXY = new int[]{ulx, uly, lrx, lry};
+        for (int i=0; i<4; i++){
+        gridXY[i] = gridXY[i]<0? 0:gridXY[i];
+        gridXY[i] = gridXY[i]>Math.pow(2,depth)? (int)Math.pow(2,depth):gridXY[i];
+        }
 
-        return new double[]{ulx, uly, lrx, lry};
+        return new int[]{gridXY[0], gridXY[1], gridXY[2], gridXY[3]};
     }
 
-    public static void main(String[] args){
-        double lrlon = -122.29705810546875;
-        double ullon = -122.2998046875;
-
-        System.out.println(getDepth(-122.2998046875, -122.29705810546875, 1000000));
-        System.out.println(Math.log(ROOT_LonDPP/((lrlon -ullon)/256)));
-        System.out.println(ROOT_LonDPP);
+    //get String[][] of figure name
+    private static String[][] getGrid(Map<String, Double> params, int depth){
+        int[] gridXY = getGridXY(params);
+        int gridLengthX = gridXY[2] - gridXY[0] + 1;
+        int gridLengthY = gridXY[3] - gridXY[1] + 1;
+        String[][] grid = new String[gridLengthY][gridLengthX];
+        for(int x = 0; x < gridLengthX; x++){
+            for (int y = 0; y < gridLengthY; y++){
+                int xx = x+gridXY[0];
+                int yy = y+gridXY[1];
+                grid[y][x] = "d"+depth+"_x"+xx+"_y"+yy+".png";
+            }
+        }
+        return grid;
     }
+
+    //get raster_ul_lon, raster_ul_lat, raster_lr_lon, raster_lr_lat
+    private static double getRaster(Map<String, Double> params, String direction){
+        int depth = getDepth(params);
+        double cellSizeLon = (ROOT_LRLON - ROOT_ULLON)/Math.pow(2,depth);
+        double cellSizeLat = (ROOT_ULLAT - ROOT_LRLAT)/Math.pow(2,depth);
+        int[] gridXY = getGridXY(params);
+        double ullon = ROOT_ULLON + cellSizeLon*gridXY[0];
+        double ullat = ROOT_ULLAT - cellSizeLat*gridXY[1];
+        double lrlon = ROOT_ULLON + cellSizeLon*(gridXY[2]+1);
+        double lrlat = ROOT_ULLAT - cellSizeLat*(gridXY[3]+1);
+
+        double value = 3.1415926; //dummy value
+        switch (direction){
+            case "ullon": value = ullon; break;
+            case "ullat": value = ullat; break;
+            case "lrlon": value = lrlon; break;
+            case "lrlat": value = lrlat; break;
+        }
+        return value;
+    }
+
+    //check results
+    private static boolean isSuccess(Map<String, Object> results, Map<String, Double> params){
+        double ullon = (double) results.get("raster_ul_lon");
+        double ullat = (double) results.get("raster_ul_lat");
+        double lrlon = (double) results.get("raster_lr_lon");
+        double lrlat = (double) results.get("raster_lr_lat");
+        int depth = (int) results.get("depth");
+
+        boolean lon = ROOT_LRLON < ullon || lrlon < ROOT_ULLON; //true while no intersection
+        boolean lat = ROOT_LRLAT > ullat || lrlat > ROOT_ULLAT; //true while no intersection
+        boolean bigger = ullon < lrlon && ullat > lrlat;
+        boolean dep = 0<= depth && depth<=7;
+        boolean input = checkInput(params);
+        return !lon && !lat && bigger && dep && input;
+    }
+
+    private static boolean checkInput(Map<String, Double> params){
+        double ullon = params.get("ullon");
+        double ullat = params.get("ullat");
+        double lrlon = params.get("lrlon");
+        double lrlat = params.get("lrlat");
+        double w = params.get("w");
+        double h = params.get("h");
+
+        boolean lon = ROOT_LRLON < ullon || lrlon < ROOT_ULLON; //true while no intersection
+        boolean lat = ROOT_LRLAT > ullat || lrlat > ROOT_ULLAT; //true while no intersection
+        boolean bigger = ullon < lrlon && ullat > lrlat;
+        boolean ww = w>0;
+        boolean hh = h>0;
+        return !lon && !lat && bigger && hh && ww;
+    }
+
 
 }
